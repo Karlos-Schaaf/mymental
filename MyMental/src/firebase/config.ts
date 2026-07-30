415d5c36c8b530579a23509714750456e396fb5d
Karlos-Schaaf <karlos.schaaf.business@gmail.com>
2026-07-30 12:58:09 +1200

    Fixed auth file API key blunder, previous changes were connecting firebase, adding login and signup pages along with authentication

--- MyMental/src/firebase/config.ts
+++ MyMental/src/firebase/config.ts
@@ -1,5 +1,4 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
@@ -12,4 +11,4 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 
-export const auth = getAuth(app);
+export default app;
